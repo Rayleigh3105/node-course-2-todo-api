df41ed5de5fc46d2b6b7138a601976f1c2f91f4b
mvogt <moritz.vogt@vogges.de>
2018-08-19 11:52:43 +0200

    Commit wo goarnix funktioniert

--- server/model/user.js
+++ server/model/user.js
@@ -1,13 +1,51 @@
-var mongoose = require('mongoose');
+const validator = require('validator')
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 
 
-var User = mongoose.model('User', {
+var UserSchema = new mongoose.Schema(
+{
     email: {
         required: true,
-        trim: true,
-        type: String,
-        minLength: 1,
+            trim: true,
+            type: String,
+            minLength: 1,
+            unique: true,
+            validate: {
+            validator: validator.isEmail,
+                message: `{VALUE} is not a valid email`
+        },
+        password: {
+            type: String,
+            require: true,
+            minLength: 6
+        },
+        tokens: [{
+            access: {
+                type: String,
+                required: true
+            },
+            token: {
+                type: String,
+                required: true
+            }
+        }]
     }
 });
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign( {_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    // user.tokens = user.tokens.concat([{access, token}]);
+    user.tokens.push({access, token})
+
+    return user.save().then( () => {
+        return token;
+    })
+};
+
+var User = mongoose.model('User', UserSchema);
 
 module.exports = { User }
