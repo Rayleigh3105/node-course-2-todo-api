a2291706d08edac0555a42e61d9a117e0147a1ef
mvogt <moritz.vogt@vogges.de>
2018-09-15 17:30:15 +0200

    Heroku testing

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 // Connect to Database
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
-module.exports = { mongoose }
+module.exports = { mongoose };
 
 
